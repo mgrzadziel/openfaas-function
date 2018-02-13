5358bdced435f9a26e73aa7574a94e24ef59669b
m.grzadziel <michal@grzadziel.pl>
2018-02-13 21:57:46 +0100

    add dummy current datetime function

---
+++ functions/csharp/CurrentDatetimeFunc/FunctionHandler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CurrentDatetimeFunc
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input) => Console.WriteLine("Current date:" + DateTime.Now.ToShortDateString());
+    }
+}
