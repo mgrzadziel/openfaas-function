8bec761826238f00fe78e337d8db2822a2be8c5a
m.grzadziel <michal@grzadziel.pl>
2018-02-13 21:30:34 +0100

    add dummy function

---
+++ functions/hello-csharp/hello-csharp/hello-csharp/FunctionHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
